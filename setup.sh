f819aa86f4c7561c9500ab45c7fb2b06e9017f2f
isucon6 <isucon6@t-lab.com>
2017-08-31 13:46:48 +0900

    fix scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget https://github.com/matsuu/kataribe/releases/download/v0.3.0/linux_amd64.zip kataribe.zip
+mkdir tmp
+wget https://github.com/matsuu/kataribe/releases/download/v0.3.0/linux_amd64.zip -O tmp/kataribe.zip
+cd tmp
 unzip kataribe.zip
+mv kataribe ../
 
 bundle install --path vendor/bundler
